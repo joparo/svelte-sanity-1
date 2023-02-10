14bf9049b4d4d9066d1db834fd196b6f53ea5414
Jonas Paro <jonas.paro@omegapoint.se>
2023-02-10 22:03:48 +0100

    Flex grow days

--- src/routes/conference/[slug]/Days.svelte
+++ src/routes/conference/[slug]/Days.svelte
@@ -5,10 +5,16 @@
 
 <div class="container flex row-auto gap-2 mx-auto justify-between mb-8">
 	{#each days as day}
-		<div class="grow p-6 bg-white border border-gray-200 shadow">
+		<div class="grow basis-1 p-6 bg-white border border-gray-200 shadow">
 			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{day.title}</h5>
 			<p class="font-normal italic text-gray-700">{day.description}</p>
 			<p class="font-normal text-sm text-gray-700">{day.date}</p>
 		</div>
 	{/each}
 </div>
+
+<style>
+	.grow {
+		flex-grow: 1;
+	}
+</style>
